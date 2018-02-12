e3c7ebc29f796acf09482ef467c40a20ed47786f
Jonathan D. Baker <jonathan@ardent.tech>
2018-02-11 17:01:58 -0700

    bulk create 2018 incidents

--- api/crime/management/commands/refresh_incidents.py
+++ api/crime/management/commands/refresh_incidents.py
@@ -1,6 +1,11 @@
+import datetime
+
 from django.core.management.base import BaseCommand
 
+from localflavor.us.us_states import STATES_NORMALIZED
+
 from crime.spiders import MassShootingSpider
+from crime.models import GVAIncident
 
 
 # @todo
@@ -36,12 +41,22 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # if no DB records, get all
         # else get delta since last record
-        self._get_mass_shootings()
+        (column_names, row_data) = self._get_mass_shootings()
+        GVAIncident.objects.bulk_create([self._row_to_object(row) for row in row_data])
 
     # PRIVATE
 
     def _get_mass_shootings(self):
         ms = MassShootingSpider(year=2018)
         ms.crawl()
-        print(ms.column_names)
-        print(ms.row_data)
+        return (ms.column_names, ms.row_data)
+
+    def _row_to_object(self, raw):
+        return GVAIncident(
+            city_county=raw[2],
+            date=datetime.datetime.strptime(raw[0], "%B %d, %Y").date(),
+            gva_id=int(raw[6]),
+            injured=int(raw[5]),
+            killed=int(raw[4]),
+            state=STATES_NORMALIZED[raw[1].lower()],
+            street=raw[3])
