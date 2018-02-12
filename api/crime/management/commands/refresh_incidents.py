ed1df850c39bd2072c00e7053fe19fd5f68e84b5
Jonathan D. Baker <jonathan@ardent.tech>
2018-02-12 14:16:00 -0700

    fetch multiple years of incidents

--- api/crime/management/commands/refresh_incidents.py
+++ api/crime/management/commands/refresh_incidents.py
@@ -6,12 +6,10 @@ from localflavor.us.us_states import STATES_NORMALIZED
 
 from crime.spiders import MassShootingSpider
 from crime.models import GVAIncident
+from taxonomy.models import Tag
 
 
-# @todo
-# use `bulk_create` to reduce DB bottle-necking
-# how to reconcile incidents appearing in multiple groups? can this be done with `bulk_create`?
-# create a report each time this is run?
+# @todo create a report each time this is run?
 
 # REPORTS
 # children killed
@@ -25,11 +23,6 @@ from crime.models import GVAIncident
 # accidental deaths (teens ages 12-17)
 # accidental injuries (teens ages 12-17)
 # officer involved shootings
-# mass shootings in 2014
-# mass shootings in 2015
-# mass shootings in 2016
-# mass shootings in 2017
-# mass shootings in 2018
 
 
 class Command(BaseCommand):
@@ -41,22 +34,42 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # if no DB records, get all
         # else get delta since last record
-        (column_names, row_data) = self._get_mass_shootings()
-        GVAIncident.objects.bulk_create([self._row_to_object(row) for row in row_data])
+        self._create("mass_shootings")
 
     # PRIVATE
 
-    def _get_mass_shootings(self):
-        ms = MassShootingSpider(year=2018)
-        ms.crawl()
-        return (ms.column_names, ms.row_data)
+    def _create(self, name, **kwargs):
+        records = getattr(self, "_create_" + name)(**kwargs)
+        print("created {0} {1}".format(len(records), name))
+        return records
 
-    def _row_to_object(self, raw):
-        return GVAIncident(
-            city_county=raw[2],
-            date=datetime.datetime.strptime(raw[0], "%B %d, %Y").date(),
-            gva_id=int(raw[6]),
-            injured=int(raw[5]),
-            killed=int(raw[4]),
-            state=STATES_NORMALIZED[raw[1].lower()],
-            street=raw[3])
+    def _create_incident(self, attrs, tag):
+        incident = GVAIncident.objects.create(**attrs)
+        incident.tags.add(tag)
+        return incident
+
+    def _create_mass_shootings(self):
+        tag = Tag.objects.get_or_create(name="mass_shooting")[0]
+        incidents = []
+
+        for year in range(2014, 2019):
+            rows = self._get_mass_shootings(year)
+            incidents += [self._create_incident(self._row_to_attrs(row), tag) for row in rows]
+
+        return incidents
+
+    def _get_mass_shootings(self, year):
+        ms = MassShootingSpider(year=year)
+        ms.crawl()
+        return ms.row_data
+
+    def _row_to_attrs(self, row):
+        return {
+            "city_county": row[2],
+            "date": datetime.datetime.strptime(row[0], "%B %d, %Y").date(),
+            "gva_id": int(row[6]),
+            "injured": int(row[5]),
+            "killed": int(row[4]),
+            "state": STATES_NORMALIZED[row[1].lower()],
+            "street": row[3]
+        }
