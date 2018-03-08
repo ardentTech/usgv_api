32734a968804e77c9d9eee79bbafbbf172b179d3
Jonathan D. Baker <jonathan@ardent.tech>
2018-03-07 19:27:10 -0700

    production settings

--- api/api/settings/pro.py
+++ api/api/settings/pro.py
@@ -1,20 +1,7 @@
 from .base import *
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ("api.usgv.ardent.tech",)
 
-CORS_ORIGIN_WHITELIST = ()
+CORS_ORIGIN_WHITELIST = ("usgv.ardent.tech",)
 
 DEBUG = False
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = from_env("USGV_EMAIL_HOST")
-EMAIL_HOST_PASSWORD = from_env("USGV_EMAIL_HOST_PASSWORD")
-EMAIL_HOST_USER = from_env("USGV_EMAIL_HOST_USER")
-
-MEDIA_ROOT = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.dirname(BASE_DIR))), "media")
-MEDIA_URL = "/media/"
-
-STATIC_ROOT = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.dirname(BASE_DIR))), "static")
-
