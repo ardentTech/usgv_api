1fbf10351ae4fcc30bf9d8246586e2d629fbee1b
Jonathan D. Baker <jonathan@ardent.tech>
2018-04-09 21:16:50 -0600

    calculate incident stats

--- api/crime/gva/stats.py
+++ api/crime/gva/stats.py
@@ -1,26 +1,112 @@
-class Stats(object):
+from enum import Enum
+from math import inf
+
+
+class Mode(Enum):
+    COUNTRY = 0
+    STATE = 1
+
+
+class Calculator(object):
 
     def __init__(self):
-        self.incidents = 0
-        self.injured = 0
-        self.killed = 0
+        self._mode = None
 
-    # @todo format(ter) arg?
-    def calculate(self, incidents):
-        self.incident = len(incidents)
+    @property
+    def mode(self):
+        return self._mode
+
+    @mode.setter
+    def mode(self, val):
+        self._mode = val
+
+    def run(self, incidents):
+        self.incidents = len(incidents)
+
+        if self.mode == Mode.STATE:
+            return self._for_state(incidents)
+        elif self.mode == Mode.COUNTRY:
+            return self._for_country(incidents)
+        else:
+            raise Exception("Unsupported calculator mode {}".format(self.mode))
+
+    # PRIVATE
+    def _for_country(self, incidents):
+        results = dict(incidents=len(incidents), least=dict(), most=dict())
+        categories = ["injured", "killed", "victims"]
+
+        for key in categories:
+            results[key] = 0
+            results["least"][key] = dict(states=[], value=inf)
+            results["most"][key] = dict(states=[], value=0)
+
+        # todo avgs
         for i in incidents:
-            self.injured += i.injured
-            self.killed += i.killed
-        return self
+            results["injured"] += i.injured
+            results["killed"] += i.killed
 
-    @property
-    def data(self):
-        return dict(
-            incidents=self.incidents,
-            injured=self.injured,
-            killed=self.killed,
-            victims=self.victims)
+            for k in categories:
+                val = getattr(i, k)
+                if val == results["least"][k]["value"]:
+                    results["least"][k]["states"].append(i.state.id)
+                elif val < results["least"][k]["value"]:
+                    results["least"][k]["value"] = val
+                    results["least"][k]["states"] = [i.state.id]
 
-    @property
-    def victims(self):
-        return self.injured + self.killed
+                if val == results["most"][k]["value"]:
+                    results["most"][k]["states"].append(i.state.id)
+                elif val > results["most"][k]["value"]:
+                    results["most"][k]["value"] = val
+                    results["most"][k]["states"] = [i.state.id]
+
+        results["victims"] = results["injured"] + results["killed"]
+        return results
+
+    def _for_state(self, incidents):
+        results = dict(incidents=len(incidents), least=dict(), most=dict())
+
+        for key in ["injured", "killed", "victims"]:
+            results[key] = 0
+
+        for i in incidents:
+            results["injured"] += i.injured
+            results["killed"] += i.killed
+
+        results["victims"] = results["injured"] + results["killed"]
+        return results
+
+#
+#            # most injured
+#            if i.injured == self.most_injured["value"]:
+#                self.most_injured["states"].append(i.state.id)
+#            elif i.injured > self.most_injured["value"]:
+#                self.most_injured["value"] = i.injured
+#                self.most_injured["states"] = [i.state.id]
+#
+#            # least killed
+#            if i.killed == stats["least_killed"]["value"]:
+#                stats["least_killed"]["states"].append(i.state.id)
+#            elif i.killed < stats["least_killed"]["value"]:
+#                stats["least_killed"]["value"] = i.killed
+#                stats["least_killed"]["states"] = [i.state.id]
+#
+#            # most killed
+#            if i.killed == stats["most_killed"]["value"]:
+#                stats["most_killed"]["states"].append(i.state.id)
+#            elif i.killed > stats["most_killed"]["value"]:
+#                stats["most_killed"]["value"] = i.killed
+#                stats["most_killed"]["states"] = [i.state.id]
+#
+#            # least victims
+#            if i.victims == stats["least_victims"]["value"]:
+#                stats["least_victims"]["states"].append(i.state.id)
+#            elif i.victims < stats["least_victims"]["value"]:
+#                stats["least_victims"]["value"] = i.victims
+#                stats["least_victims"]["states"] = [i.state.id]
+#
+#            # most victims
+#            if i.victims == stats["most_victims"]["value"]:
+#                stats["most_victims"]["states"].append(i.state.id)
+#            elif i.victims > stats["most_victims"]["value"]:
+#                stats["most_victims"]["value"] = i.victims
+#                stats["most_victims"]["states"] = [i.state.id]
--- api/crime/views.py
+++ api/crime/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
 from .filters import GVAIncidentFilter
-from .gva.stats import Stats
+from .gva.stats import Calculator, Mode
 from .models import GVAIncident
 from .serializers import GVAIncidentSerializer, GVAIncidentStatsSerializer
 
@@ -48,81 +48,15 @@ class GVAIncidentViewSet(ListModelMixin, GenericViewSet):
 
 class GVAStats(APIView):
 
-    # @todo stats generator class
     def get(self, request, format=None):
         filters = dict(date__year=request.query_params.get("year"))
-        stats = Stats()
+        calculator = Calculator()
+        calculator.mode = Mode.COUNTRY
 
         state = request.query_params.get("state", None)
         if state is not None:
             filters["state"] = state
-            # @todo update stats mode
+            calculator.mode = Mode.STATE
 
         incidents = GVAIncident.objects.filter(**filters)
-        stats.calculate(incidents)
-
-        return Response(stats.data)
-
-#        filters = dict(date__year=2018)
-#        incidents = GVAIncident.objects.filter(**filters)
-#        stats = dict(
-#            incidents=0, injured=0, killed=0, victims=0,
-#            least_injured=dict(states=[], value=math.inf),
-#            most_injured=dict(states=[], value=0),
-#            least_killed=dict(states=[], value=math.inf),
-#            most_killed=dict(states=[], value=0),
-#            least_victims=dict(states=[], value=math.inf),
-#            most_victims=dict(states=[], value=0),
-#        )
-#
-#        for i in incidents:
-#            stats["injured"] += i.injured
-#            stats["killed"] += i.killed
-#
-#            # @todo only calculate if state is None
-#            # least injured
-#            if i.injured == stats["least_injured"]["value"]:
-#                stats["least_injured"]["states"].append(i.state.id)
-#            elif i.injured < stats["least_injured"]["value"]:
-#                stats["least_injured"]["value"] = i.injured
-#                stats["least_injured"]["states"] = [i.state.id]
-#
-#            # most injured
-#            if i.injured == stats["most_injured"]["value"]:
-#                stats["most_injured"]["states"].append(i.state.id)
-#            elif i.injured > stats["most_injured"]["value"]:
-#                stats["most_injured"]["value"] = i.injured
-#                stats["most_injured"]["states"] = [i.state.id]
-#
-#            # least killed
-#            if i.killed == stats["least_killed"]["value"]:
-#                stats["least_killed"]["states"].append(i.state.id)
-#            elif i.killed < stats["least_killed"]["value"]:
-#                stats["least_killed"]["value"] = i.killed
-#                stats["least_killed"]["states"] = [i.state.id]
-#
-#            # most killed
-#            if i.killed == stats["most_killed"]["value"]:
-#                stats["most_killed"]["states"].append(i.state.id)
-#            elif i.killed > stats["most_killed"]["value"]:
-#                stats["most_killed"]["value"] = i.killed
-#                stats["most_killed"]["states"] = [i.state.id]
-#
-#            # least victims
-#            if i.victims == stats["least_victims"]["value"]:
-#                stats["least_victims"]["states"].append(i.state.id)
-#            elif i.victims < stats["least_victims"]["value"]:
-#                stats["least_victims"]["value"] = i.victims
-#                stats["least_victims"]["states"] = [i.state.id]
-#
-#            # most victims
-#            if i.victims == stats["most_victims"]["value"]:
-#                stats["most_victims"]["states"].append(i.state.id)
-#            elif i.victims > stats["most_victims"]["value"]:
-#                stats["most_victims"]["value"] = i.victims
-#                stats["most_victims"]["states"] = [i.state.id]
-#
-#        stats["incidents"] = len(incidents)
-#        stats["victims"] = stats["injured"] + stats["killed"]
-#
-#        return Response(json.dumps(stats))
+        return Response(calculator.run(incidents))
